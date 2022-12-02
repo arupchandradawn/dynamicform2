11a0460b6236a9986a17f49f66cbce241e294130
arupchandradawn <arup.chandra@aspiresys.com>
2022-12-02 14:11:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import jsonfile from './dynamic.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const jsonValue = require ("./dynamic.json");
+  console.log("jsonValue >>",jsonValue);
+
+  const [finalJsonValue,setfinalJsonValue] = useState(jsonValue);
+
+  const handleFormChange = (event,index) =>{
+    let data = [...jsonValue];
+    data[index]['value'] = event.target.value;
+    console.log("event.target.value>>",data[index]);
+    setfinalJsonValue(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {finalJsonValue.map((field,index) => {
+          return<div key={index} className="mainClass">
+            <div>
+              {field.label}
+            </div>
+            {
+              (field.labeltype === "Textbox") && (<input 
+                value={field.value}
+                onChange={event => handleFormChange(event,index)}
+              ></input>)
+            }
+            {
+              (field.labeltype === "DropDownlist") && (<select>
+                <option>{field.value}</option>
+              </select>)
+            }
+          </div>
+        })}
+        <br/><button>Save</button>
     </div>
   );
 }
